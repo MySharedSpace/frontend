814fbad97487696fb80a369ea0496d1ed8328065
MSSapp <147052042+MSSapp@users.noreply.github.com>
2023-11-17 19:54:25 +0300

    Update global.js

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 
 //The url should not have a slash at the end
-const backend_server = "https://mssbackend.onrender.com"
+const backend_server = "http://20.68.243.37:5000"
 
 
 export default {backend_server}
